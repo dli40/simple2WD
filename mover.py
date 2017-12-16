7d498251c1b2c05c0f16139a3ee18c6d6848b1f7
Daniel Li <dli40@calpoly.edu>
2017-12-15 18:41:25 -0800

    Update mover.py

--- mover.py
+++ mover.py
@@ -4,17 +4,18 @@ import explorerhat
 	
 for i in range(0,5): #forward
 	explorerhat.motor.one.forwards()
-	print("motor one going forwards")
 	explorerhat.motor.two.forwards()
-	print("motor two going forwards")    
 print("finished going forwards")
 for i in range(0,5): #reverse
 	explorerhat.motor.one.backwards()
 	explorerhat.motor.two.backwards()
+print("finished going backwards")	
 for i in range(0,5): #right
 	explorerhat.motor.one.forwards()
 	explorerhat.motor.two.backwards()
+print("finished going left")	
 for i in range(0,5): #left
 	explorerhat.motor.one.backwards()
-	explorerhat.motor.two.forwards()	
+	explorerhat.motor.two.forwards()
+print("finished going right")	
 	
