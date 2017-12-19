dadc6dcfdc0fc5d9f86c9cf8d18e8a71d17c2a67
Daniel Li <dli40@calpoly.edu>
2017-12-19 10:09:21 -0800

    Update mover.py

--- mover.py
+++ mover.py
@@ -1,20 +1,17 @@
+#property of Daniel Li: https://github.com/dli40
 import explorerhat
 
 #test code
 	
 explorerhat.motor.two.invert() #because this motor is backwards
 	
-for i in range(0,9999):
+for i in range(0,9999): #Will go forwards for 10,000 steps. It's about 5 seconds of motion. 
 	explorerhat.motor.forwards()
 print("done with forwards")
 explorerhat.motor.stop()
 
-for i in range(0,9999):
+for i in range(0,9999): #now go backwards
 	explorerhat.motor.backwards()
-	
-	
-	
-	
-	
+		
 explorerhat.motor.two.invert() #reset all changes before exiting	
 
