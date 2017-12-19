345f5823505450789e78ff4ef1d2281dc8ab5cfd
Daniel Li <dli40@calpoly.edu>
2017-12-18 23:18:29 -0800

    Update mover.py

--- mover.py
+++ mover.py
@@ -5,7 +5,7 @@ import explorerhat
 
 	
 for i in range(0,9999):
-	explorerhat.motor.one.forwards()
+	explorerhat.motor.one.backwards()
 	explorerhat.motor.two.forwards()
 print("done with forwards")
 explorerhat.motor.stop()
